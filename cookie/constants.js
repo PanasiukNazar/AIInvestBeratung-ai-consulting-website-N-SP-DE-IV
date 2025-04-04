6770feadeadfc2f9cb7106cf305983207ae84a93
Nazar Panasiuk <nazar.panasiuk.ua@gmail.com>
2025-04-04 08:13:05 +0300

    WIP

--- cookie/constants.js
+++
@@ -1,9 +0,0 @@
-const COOKIE_TYPES = ["essential", "performance", "advertising"];
-const DAY = 1;
-const COOKIE_NAMES = {
-    essential: "essential_cookie",
-    performance: "performance_cookie",
-    advertising: "advertising_cookie"
-};
-
-export { COOKIE_TYPES, COOKIE_NAMES, DAY };
